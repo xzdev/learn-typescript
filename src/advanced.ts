a946274b0cac87f7aa32d10aabc75bf0a2d12b00
Xiaoyu Zhang <xzhang@moveworks.ai>
2020-04-09 13:12:10 -0700

    Update advanced typescript example

--- src/advanced.ts
+++ src/advanced.ts
@@ -1,5 +1,144 @@
 
-// use never to check exhaustive in switch
+// 1. Use Generics
+
+// example 1
+class AnyStorage<T> {
+  private storage: Map<string, T> = new Map<string, T>();
+  public saveToDataStore(key: string, value: T): void {
+    this.storage.set(key, value);
+  };
+  public getFromDataStoer(key: string): T | undefined {
+    return this.storage.get(key);
+  }
+}
+
+// example 2
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+  console.log(arg.length);  // Now we know it has a .length property, so no more error
+  return arg;
+}
+
+// example 3
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, "a"); // okay
+getProperty(x, "m"); // error:
+
+// 2. Use union type
+
+// example 1
+function padLeftBad(value: string, padding: any) {
+  if (typeof padding === "number") {
+    return Array(padding + 1).join(" ") + value;
+  }
+  if (typeof padding === "string") {
+    return padding + value;
+  }
+  throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+function padLeftGood(value: string, padding: string | number) {
+  // ...
+}
+
+let indentedString = padLeftGood("Hello world", true); // errors during compilation
+
+// example 2
+
+type ExampleResponse = {
+  loading: boolean;
+  error: boolean;
+  payload?: object;
+};
+
+// { loading: true, error: false } → 'LOADING'
+// { loading: false, error: false } → 'SUCCESS'
+// { loading: false, error: true } → 'ERROR'
+// { loading: true, error: true } → should never happen!
+
+type SuccessfulResponse = {
+  status: 'SUCCESS';
+  payload: object;
+}
+
+type ResponseInProgress = {
+  status: 'LOADING';
+};
+
+type ErrorResponse = {
+  status: 'ERROR';
+};
+
+type SomeResponse = ResponseInProgress | SuccessfulResponse | ErrorResponse;
+
+// 3. Enum and string literals
+type Coin = 'quarter' | 'dime' | 'nickle' | 'penny';
+enum CoinEnum {
+  Quarter = 'quarter',
+  Dime = 'dime',
+  Nickle = 'nickle',
+  Penny = 'penny',
+}
+
+
+// 4. Strict null & undefined check
+
+// Compiled with --strictNullChecks
+let x: number;
+let y: number | null;
+let z: number | undefined;
+x;  // Error, reference not preceded by assignment
+y;  // Error, reference not preceded by assignment
+z;  // Ok
+x = 1;
+y = null;
+x;  // Ok
+y;  // Ok
+
+// 5. Use type alias
+type UserId = string;
+type OrgName = string;
+
+// 6. Use predicate
+
+interface Bird {
+  fly(): void;
+  layEggs(): void;
+}
+
+interface Fish {
+  swim(): void;
+  layEggs(): void;
+}
+
+function getSmallPet(): Fish | Bird {
+  // ...
+}
+
+let pet = getSmallPet();
+pet.layEggs(); // okay
+pet.swim();    // errors
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+if (isFish(pet)) {
+  pet.swim();
+}
+else {
+  pet.fly();
+}
+
+// 7. use never to check exhaustive in switch
 
 enum Color {
   Red,
@@ -13,7 +152,7 @@ function getColorName(c: Color): string {
       return 'red';
     case Color.Green:
       return 'green';
-      // Forgot about Blue
+    // Forgot about Blue
   }
 
   throw new Error('Did not expect to be here');
@@ -28,66 +167,7 @@ function getColorName2(c: Color): string {
       return 'red';
     case Color.Green:
       return 'green';
-      // Forgot about Blue
+    // Forgot about Blue
   }
   return assertUnreachable(c);
 }
-
-
-// Discriminated union using string literals
-interface Dog {
-  species: 'canine';
-  woof: string;
-}
-interface Cat {
-  species: 'feline';
-  meow: string;
-}
-interface Fish {
-  species: 'pisces';
-  meow: string;
-}
-type Pet = Dog|Cat|Fish;
-
-// Externally-visible signature
-function throwBadPet(p: never): never;
-// Implementation signature
-function throwBadPet(p: Pet) {
-  throw new Error('Unknown pet kind: ' + p.species);
-}
-
-function meetPet(p: Pet) {
-  switch (p.species) {
-    case 'canine':
-      console.log('Who\'s a good boy? ' + p.woof);
-      break;
-    case 'feline':
-      console.log('Pretty kitty: ' + p.meow);
-      break;
-    // case 'pisces':
-    //   console.log('Baby shark: ' + p.meow);
-    //   break;
-    default:
-      // Argument of type 'Fish' not assignable to 'never'
-      throwBadPet(p);
-  }
-}
-
-// Hybrid Types
-interface Counter {
-  (start: number): string;
-  interval: number;
-  reset(): void;
-}
-
-function getCounter(): Counter {
-  let counter = (function(start: number) {}) as Counter;
-  counter.interval = 123;
-  counter.reset = function() {};
-  return counter;
-}
-
-let c = getCounter();
-c(10);
-c.reset();
-c.interval = 5.0;
